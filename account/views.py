864667bac308c7025f00683b0e65aa236738218c
khadetou <khadetou96@gmail.com>
2023-08-06 19:50:28 +0000

    contact

--- account/views.py
+++ account/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from contact.models import Contact
 # Create your views here.
--- btre/settings.py
+++ btre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +142,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# EMAIL CONFIG
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'khadetou96@gmail'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
