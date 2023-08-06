ca5f5fde528b0b527f65330f8e8ff5bc112181aa
khadetou <khadetou96@gmail.com>
2023-08-06 20:20:15 +0000

    account views

--- btre/settings.py
+++ btre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'contact.apps.ContactConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
