de31be3086d8d5a9bd2760e56a52d66208549844
khadetou <khadetou96@gmail.com>
2023-08-06 15:53:16 +0000

    listing pages

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'listings/index.html')
+
+
+def listing(request):
+    return render(request, 'listings/listing.html')
+
+
+def search(request, listing_id):
+    return render(request, 'listings/search.html')
