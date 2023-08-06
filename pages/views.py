e87419c2d1d7dc480ab591153e84f5ca43073632
khadetou <khadetou96@gmail.com>
2023-08-06 16:51:58 +0000

    complete page views

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-
+from listings.choices import state_choices, bedroom_choices, price_choices
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
